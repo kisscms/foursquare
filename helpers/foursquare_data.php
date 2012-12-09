117201c97ce50869b8bd176793c05e3d60c775ef
Makis Tracend <makis.tracend@gmail.com>
2012-12-09 01:59:51 -0800

    Adding data controller

---
+++ helpers/foursquare_data.php
@@ -0,0 +1,35 @@
+<?php
+
+// FIX - force include the base class
+require_once( getPath("api/helpers/rest_service.php") );
+
+// load the data of a file or user...
+class Foursquare_Data extends REST_Service {
+	
+	function __construct($controller_path,$web_folder,$default_controller,$default_function)  {
+		
+		// main objects
+		$this->api['foursquare'] = new Foursquare();
+		
+		// continue to the default setup
+		parent::__construct($controller_path,$web_folder,$default_controller,$default_function);
+		
+	}
+	
+	function index( $params ) {
+		// get the path 
+		$path =  str_replace("/index.php/data/", "", $_SERVER['PHP_SELF']);
+		// pop the first pair in the params
+		array_shift( $params );
+		
+		$method = $_SERVER['REQUEST_METHOD'];
+		
+		$this->data = $this->api['foursquare']->{$method}( $path, $params );
+		
+		$this->render();
+		
+	}
+	
+}
+
+?>
