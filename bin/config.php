e6b147930e895def3bf77902689b1874f022eff0
Makis Tracend <makis.tracend@gmail.com>
2013-05-20 04:27:50 -0700

    adding config option for client side authentication

--- bin/config.php
+++ bin/config.php
@@ -10,6 +10,7 @@ if( class_exists('Config') && method_exists(new Config(),'register')){
 	// Register variables
 	Config::register("foursquare", "key", "0000000");
 	Config::register("foursquare", "secret", "AAAAAAAAA");
+	Config::register("foursquare", "client_auth", 0);
 
 }
 
