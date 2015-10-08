b1139f2a760054e3f484ca39cad1978231d56a03
Makis Tracend <makis.tracend@gmail.com>
2015-10-08 00:50:26 -0700

    custom checkToken method

--- helpers/foursquare_oauth.php
+++ helpers/foursquare_oauth.php
@@ -4,40 +4,46 @@ require_once( getPath("oauth/helpers/kiss_oauth.php") );
 
 /* Discus for KISSCMS */
 class Foursquare_OAuth extends KISS_OAuth_v2 {
-	
+
 	function  __construct( $api="foursquare", $url="https://foursquare.com/oauth2" ) {
-		
+
 		$this->url = array(
-			'authorize' 		=> $url ."/authenticate", 
-			'access_token' 		=> $url ."/access_token", 
+			'authorize' 		=> $url ."/authenticate",
+			'access_token' 		=> $url ."/access_token",
 		);
-		
+
 		parent::__construct( $api, $url );
-		
+
 	}
-	
+
 	// - Access a token given a code (GET method)
 	function access_token( $params, $request=array() ){
-		
+
 		$request = array(
 			"params" => array("grant_type" => "authorization_code")
 		);
-		
+
 		parent::access_token($params, $request);
 
 	}
-	
+
 	function save( $response ){
-		
+
 		// erase the existing cache
 		//$foursquare = new Foursquare();
 		//$foursquare->deleteCache();
-		
+
 		// save to the user session (decode string and produce an array)
 		$_SESSION['oauth']['foursquare'] = (array) json_decode($response);
-		
+
 	}
-	
+
+	// Foursquare doesn't send an expiry key... need to customize the check
+	function checkToken(){
+		//
+		return ( empty($_SESSION['oauth'][$this->api]['access_token']) );
+	}
+
 }
 
 ?>
